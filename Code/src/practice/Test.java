59bf3fa8aedbe309c5fc272d67606660c13a1bae
himanshuv <himanshu@contaque.com>
2022-12-29 10:39:18 +0530

    some test file added

--- Code/src/practice/Test.java
+++ Code/src/practice/Test.java
@@ -1,22 +1,29 @@
 package practice;
 
 public class Test {
+
+
+    Test(int x){
+        System.out.println("parameter");
+    }
     public static void main(String[] args) {
-        try {
-            int n = 1000, x = 0;
-            int arr[] = new int[n];
-            for (int i = 0; i <= n; i++) {
-                arr[i] = i / x;
-            }
-        }
-        catch (ArrayIndexOutOfBoundsException exception) {
-            System.out.println("1st block = ArrayIndexOutOfBoundsException");
-        }
-        catch (ArithmeticException exception) {
-            System.out.println("2nd block = ArithmeticException");
-        }
-        catch (Exception exception) {
-            System.out.println("3rd block = Exception");
-        }
+
+        Test t = new Test(1);
+//        try {
+//            int n = 1000, x = 0;
+//            int arr[] = new int[n];
+//            for (int i = 0; i <= n; i++) {
+//                arr[i] = i / x;
+//            }
+//        }
+//        catch (ArrayIndexOutOfBoundsException exception) {
+//            System.out.println("1st block = ArrayIndexOutOfBoundsException");
+//        }
+//        catch (ArithmeticException exception) {
+//            System.out.println("2nd block = ArithmeticException");
+//        }
+//        catch (Exception exception) {
+//            System.out.println("3rd block = Exception");
+//        }
     }
 }
